0d8155abef741180784f1a6865664d5b591a9e5d
yuanzhihai <396751927@qq.com>
2023-02-09 10:31:13 +0800

    兼容 thinkphp8

--- src/helper.php
+++ src/helper.php
@@ -5,9 +5,9 @@ use thans\jwt\provider\JWT as JWTProvider;
 use think\facade\Console;
 use think\App;
 
-if (strpos(App::VERSION, '6') === false) {
-    Console::addCommands([
+if (App::VERSION <= '6.0.0') {
+    Console::addCommands( [
         SecretCommand::class
-    ]);
-    (new JWTProvider(app('request')))->init();
+    ] );
+    ( new JWTProvider( app( 'request' ) ) )->init();
 }
