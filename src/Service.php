6a8c4e1cc6957eaf664cf10b92875136e9c6e9c3
yuanzhihai <396751927@qq.com>
2023-03-06 15:34:39 +0800

    完善

--- src/Service.php
+++ src/Service.php
@@ -4,13 +4,27 @@
 namespace thans\jwt;
 
 use thans\jwt\command\SecretCommand;
+use thans\jwt\guard\Jwt;
 use thans\jwt\middleware\InjectJwt;
 
 class Service extends \think\Service
 {
     public function boot()
     {
-        $this->commands(SecretCommand::class);
-        $this->app->middleware->add(InjectJwt::class);
+        $this->commands( SecretCommand::class );
+        $this->app->middleware->add( InjectJwt::class );
+
+        $this->app->get( 'auth' )->extend( 'jwt',function ($app,$name,array $config) {
+            return new Jwt(
+                $this->app->get( 'thans.jwt' ),
+                $this->app->get( 'auth' )->createUserProvider( $config['provider'] ),
+                $this->app->request
+            );
+        } );
+    }
+
+    public function register()
+    {
+        $this->app->bind( 'thans.jwt',\thans\jwt\JWT::class );
     }
 }
